892170208a36ef9ed7e6d132cd6c8afb4f4dc25c
Petzh <ziphirum@gmail.com>
2013-10-07 21:16:05 +0700

    Add timeout for TAIWAN!!!

--- js/script.js
+++ js/script.js
@@ -39,3 +39,34 @@ jQuery(document).ready(function($){
 				input.value = input.defaultValue;
 				}
 			};
+
+var targetDateStr = "Oct 18, 2013";
+		// set the date we're counting down to
+		var target_date = new Date(targetDateStr).getTime();
+		// variables for time units
+		var days, hours, minutes, seconds;
+		// get tag element
+		var countdown = document.getElementById("countdown");
+		// update the tag with id "countdown" every 1 second
+		setInterval(function () {
+		    // find the amount of "seconds" between now and target
+		    var current_date = new Date().getTime();
+		    var seconds_left = (target_date - current_date) / 1000;
+		    // do some time calculations
+		    days = parseInt(seconds_left / 86400);
+		    seconds_left = seconds_left % 86400;
+		     
+		    hours = parseInt(seconds_left / 3600);
+		    seconds_left = seconds_left % 3600;
+		     
+		    minutes = parseInt(seconds_left / 60);
+		    seconds = parseInt(seconds_left % 60);
+		    
+			if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+			   countdown.innerHTML = "";
+			} else {
+			    // format countdown string + set tag value
+			    countdown.innerHTML = "<span class='span-timeout'>" + days + "</span> DAYS,  <span class='span-timeout'>" + hours + "</span> HOURS,<span class='span-timeout'> "
+			    + minutes + "</span> MINS, <span class='span-timeout'>" + seconds + " </span> SECS";
+			}
+		}, 1000);
