a3e3d18abd50d09ccddaf2c9034fea4411e6a3fa
ziphirum <ziphirum@gmail.com>
2014-09-10 22:55:55 +0700

    .

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ var Petzh = angular.module('Petzh', ['ngRoute']);
 
 
 Petzh.config(function ($routeProvider) {
-	$routeProvider.
-	when("/home",{ 
+	$routeProvider
+	.when("/home",{ 
 		controller: "HomeCtrl",
 		templateUrl: "/html/home.html",
-	}).
-	when("/dont-touch-the-white",{ 
+	})
+	.when("/dont-touch-the-white",{ 
 		controller: "DontTouchTheWhiteCtrl",
 		templateUrl: "/html/dont-touch.html",
-	}).
-	when("/petch-love-ploy",{ 
+	})
+	.when("/petch-love-ploy",{ 
 		controller: "BaseCtrl",
 		templateUrl: "/html/petch-love-ploy.html",
-	}).
-	/*otherwise({
+	})
+	/*.otherwise({
 		redirectTo : "/dont-touch-the-white"
 	});*/
 });
