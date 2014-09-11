0422189a50d6e00eec12d241f29ea54dab9a62c9
ziphirum <ziphirum@gmail.com>
2014-09-12 01:27:04 +0700

    all

--- js/controller.js
+++ js/controller.js
@@ -8,8 +8,42 @@ Petzh.controller("RootCtrl", function($scope, $route) {
 });
 
 
-Petzh.controller("DontTouchCtrl", function($scope) {
+Petzh.controller("DontTouchCtrl", function($scope, $interval) {
 
-	console.log("DontTouchCtrl");
+	$scope.timer = 60;
+	$scope.score = 0;
 
+	var stop;
+	$scope.startTimer = function() {
+        // Don't start a new fight if we are already fighting
+        if ( angular.isDefined(stop) ) return;
+
+        stop = $interval(function() {
+
+        	if ($scope.timer > 0) {
+        		$scope.timer -= 1;
+        	} else {
+        		$scope.stopTimer();
+        	}
+        }, 1000);
+    };
+
+    $scope.stopTimer = function() {
+    	if (angular.isDefined(stop)) {
+    		$interval.cancel(stop);
+    		stop = undefined;
+    	}
+    };
+
+    $scope.resetTimer = function() {
+    	$scope.timer = 60;
+    	$scope.stopTimer();
+    	// $scope.blood_1 = 100;
+    	// $scope.blood_2 = 120;
+    };
+
+    $scope.$on('$destroy', function() {
+      	// Make sure that the interval is destroyed too
+		$scope.stopTimer();
+  	});
 });
