558c5c318d657800e6cf968b34ef20bc96446f11
ziphirum <ziphirum@gmail.com>
2014-09-13 16:38:00 +0700

    add logic to reset block

--- js/app.js
+++ js/app.js
@@ -33,29 +33,72 @@ Petzh.directive("dontTouch", function($timeout){
 	return {
 		restrict: "A",
 		link: function (scope, element, attrs) {
+			scope.$on("resetBlock", function(event){
+				alert("reset");
+				event.preventDefault();
+				event.stopPropagation();
+			});
+
 			element.on("click", function(){
 				select(element);
 			});
 			//manipulate grids here
 			var ctrl = element.controller();
+			var blocks = $(".block");
+			var blockNums = [];
+			var numbers = [1,2,3,4,5,6,7,8,9];
+
+			function getBlockList(amount){
+				var list = [];
+				var _numbers = angular.copy(numbers);
+				for (var x=0 ; x<amount ; x++){
+					var index = _.random(_numbers.length - 1);
+					var n = _numbers.splice(index, 1);
+					list.push(n[0]);
+				}
+				return list;
+			}
+
 			function select(grid){
-				var blocks = $(".block");
-				var blockNum = getRandomInt(1,10); //1-9
+				blockNums = getBlockList(scope.whiteBlock);
 				if (grid.hasClass("beware")){
 					alert("GAME OVER");
 				} else {
 					$(".block").removeClass("beware");
-					$("#block-" + blockNum).addClass("beware");
+					angular.forEach(blockNums, function(num){
+						$("#block-" + num).addClass("beware");
+					});
+
+					if (scope.score < 5){
+						scope.whiteBlock = 1;
+						scope.timer += 100;
+					} else if (scope.score < 10){
+						scope.whiteBlock = 2;
+						scope.timer += 90;
+					} else if (scope.score < 15){
+						scope.whiteBlock = 3;
+						scope.timer += 80;
+					} else if (scope.score < 20){
+						scope.whiteBlock = 4;
+						scope.timer += 70;
+					} else if (scope.score < 25){
+						scope.whiteBlock = 5;
+						scope.timer += 60;
+					} else if (scope.score < 30){
+						scope.whiteBlock = 6;
+						scope.timer += 50;
+					} else if (scope.score < 35){
+						scope.whiteBlock = 7;
+						scope.timer += 40;
+					} else if (scope.score < 40){
+						scope.whiteBlock = 8;
+						scope.timer += 30;
+					}
 
 					$timeout(function(){
 						ctrl.addScore();
 					})
 				}
-			}
-
-
-			function getRandomInt(min, max) {
-				return Math.floor(Math.random() * (max - min)) + min;
 			}
 
 		}
--- js/controller.js
+++ js/controller.js
@@ -6,12 +6,16 @@ Petzh.controller("RootCtrl", function($scope, $route) {
 
 
 Petzh.controller("DontTouchCtrl", function($scope, $interval) {
-
-	$scope.timer = 60;
+	
+    $scope.modifier = 10;
+    $scope.defaultTime = 60 * $scope.modifier;
+    $scope.timer = angular.copy($scope.defaultTime);
+    $scope.whiteBlock = 1;
 	$scope.score = 0;
+    $scope.ready = false;
 
 	var stop;
-	$scope.startTimer = function() {
+	this.startTimer = function() {
         // Don't start a new fight if we are already fighting
         if ( angular.isDefined(stop) ) return;
 
@@ -22,26 +26,26 @@ Petzh.controller("DontTouchCtrl", function($scope, $interval) {
         	} else {
         		$scope.stopTimer();
         	}
-        }, 1000);
-    };
+        }, 100);
+    };$scope.startTimer = this.startTimer;
 
-    $scope.stopTimer = function() {
+    this.stopTimer = function() {
     	if (angular.isDefined(stop)) {
     		$interval.cancel(stop);
     		stop = undefined;
     	}
-    };
+    };$scope.stopTimer = this.stopTimer;
 
-    $scope.resetTimer = function() {
-    	$scope.timer = 60;
+    this.resetTimer = function() {
+    	$scope.timer = $scope.defaultTime;
     	$scope.stopTimer();
-    };
+        $scope.$broadcast("resetBlock");
+    };$scope.resetTimer = this.resetTimer;
 
     //Public function
     this.addScore = function(){
         $scope.score++;
-    }
-    $scope.addScore = this.addScore;
+    };$scope.addScore = this.addScore;
 
     $scope.$on('$destroy', function() {
 		$scope.stopTimer();
